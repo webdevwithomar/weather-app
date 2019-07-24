6fb2048620676971cd929aa4eb88c2cb27381934
Omar Faruque <omar.faruque967@gmail.com>
2019-07-24 17:15:55 +0600

    app.js port change

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const hbs = require('hbs');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 // path
